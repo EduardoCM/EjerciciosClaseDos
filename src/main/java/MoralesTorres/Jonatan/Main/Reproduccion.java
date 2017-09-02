dac0c3d4d18d04e2c12307ceaf75dd02b9a14f54
JonyDMorales <jonatan_moralest@hotmail.com>
2017-09-02 14:39:33 -0500

    Commit de Jona

--- src/main/java/MoralesTorres/Jonatan/Main/Reproduccion.java
+++ src/main/java/MoralesTorres/Jonatan/Main/Reproduccion.java
@@ -5,18 +5,21 @@ import MoralesTorres.Jonatan.Persona;
 public class Reproduccion {
     public static void main(String[] args) {
         Persona Juan = new Persona();
-        Juan.nombre = "Juan";
-        Juan.sexo = "Hombre";
+        Juan.setNombre("Juan");
+        Juan.setSexo("Hombre");
         
         Persona Pedro = new Persona();
-        Pedro.nombre = "Pedro";
-        Pedro.sexo = "Hombre";
+        Pedro.setNombre("Pedro");
+        Pedro.setSexo("Hombre");
         
         Persona Maria = new Persona();
-        Maria.nombre = "Maria";
-        Maria.sexo = "Mujer";
+        Maria.setNombre("Maria");
+        Maria.setSexo("Mujer");
         
-        Juan.reproducirse(Pedro);
-        Juan.reproducirse(Maria);
+        Juan.reproducirse(null);
+        Persona hijo = Maria.reproducirse(Juan);
+        
+        System.out.println("Nombre del padre: " + hijo.getPadre().getNombre());
+        System.out.println("Nombre de la madre: " + hijo.getMadre().getNombre());
     }
 }
--- src/main/java/MoralesTorres/Jonatan/Persona.java
+++ src/main/java/MoralesTorres/Jonatan/Persona.java
@@ -1,27 +1,62 @@
 package MoralesTorres.Jonatan;
 
 public class Persona {
-    public String nombre;
-    public String sexo;
+    private String nombre;
+    private String sexo;
     
     Persona madre;
     Persona padre;
     
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Persona getMadre() {
+        return madre;
+    }
+
+    public void setMadre(Persona madre) {
+        this.madre = madre;
+    }
+
+    public Persona getPadre() {
+        return padre;
+    }
+
+    public void setPadre(Persona padre) {
+        this.padre = padre;
+    }
+    
     public Persona reproducirse(Persona persona){
         Persona hijo = null;
-        if(this.sexo.equals(persona.sexo)){
+        if(persona == null){
+            System.out.println("Error");
+        }else if(this.sexo.equals(persona.sexo)){
             System.out.println("No se reproducieron exitosamente");
         }else{
-            //try{
-                hijo = new Persona();
-                //this.padre = this;
-                //this.madre = persona;
+            hijo = new Persona();
+            if(this.sexo.equals("Hombre")){
                 hijo.padre = this;
                 hijo.madre = persona;
-                hijo.nombre = "JonyD";
-                hijo.sexo = "Masculino";
-                System.out.println("Se reproducieron exitosamente");
-            //}catch(Exception e){}
+            }else{
+                hijo.padre = persona;
+                hijo.madre = this;
+            }
+            hijo.nombre = "JonyD";
+            hijo.sexo = "Masculino";
+            System.out.println("Se reproducieron exitosamente");
         }
         return hijo;
     }
