5ae6bc9b518b196f7a7117bed6827aa979e04669
vicpack <vicpackboss@gmail.com>
2017-09-02 13:24:14 -0500

    commit de vickpack

---
+++ src/main/java/moraleszepeda/victor/MainPerro.java
@@ -0,0 +1,13 @@
+
+package moraleszepeda.victor;
+
+
+public class MainPerro {
+    public static void main(String []args){
+        Perro firulais = new Perro();
+        firulais.obtenerNombre("Firualis");
+        firulais.mostrarEdad(3);
+        firulais.vacunas(6);
+        firulais.raza("Pastor Belga");
+    }
+}
