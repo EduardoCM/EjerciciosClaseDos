2dc08950cc3b7568096aa7d0eaa86f97d40988f1
JonyDMorales <jonatan_moralest@hotmail.com>
2017-09-02 15:30:09 -0500

    Commit de Jona

--- src/main/java/MoralesTorres/Jonatan/Animales.java
+++ src/main/java/MoralesTorres/Jonatan/Animales.java
@@ -2,15 +2,26 @@ package MoralesTorres.Jonatan;
 
 public class Animales {
     public static void main(String[] args) {
-        Caballo caballo = new Caballo();
+        Animal caballo = new Caballo();
         caballo.nombre = "Spirit";
         caballo.especie = "Mustang";
         caballo.comer("Pasto");
         
-        Leon leon = new Leon();
+        Animal leon = new Leon();
         leon.nombre = "Simba";
         leon.comer("Carne");
         leon.dormir();
+        
+        System.out.println(adoptarMascota(leon));
     }
- 
+    
+    public static String adoptarMascota(Animal animal){
+        String instrucciones = "Instrucciones: ";
+        if(animal instanceof Leon){
+            instrucciones += "leon come mucho";
+        }else if(animal instanceof Caballo){
+            instrucciones += "caballo muchos cuidados";
+        }
+        return instrucciones;
+    }
 }
